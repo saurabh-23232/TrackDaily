8bc7471720d5b23398f998c9d08d8a1bd050f42a
Saurabh Kumar <76238822+saurabh-23232@users.noreply.github.com>
2025-06-19 00:26:49 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
